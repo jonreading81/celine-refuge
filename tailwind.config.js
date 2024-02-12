6d5f7b1a2a52e81dbe8d029d44516a930ccb806f
Jon Reading <jon.reading@mixcloud.com>
2024-02-12 11:49:50 +0000

    chore: add config manually

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require('@material-tailwind/react/utils/withMT');
-
-module.exports = withMT({
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontSize: {
@@ -26,4 +28,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+};
