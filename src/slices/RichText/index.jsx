ecaf01a32b7d137ab842d543973cd3402f7737c6
Jon Reading <jon.reading@mixcloud.com>
2024-02-15 14:24:51 +0000

    chore: update rich text styling

--- src/slices/RichText/index.jsx
+++ src/slices/RichText/index.jsx
@@ -12,9 +12,16 @@ const components = {
       return <code>{children}</code>;
     }
   },
-  paragraph: ({ children }) => <p className="mb-8 text-md">{children}</p>,
-  heading1: ({ children }) => <h1 className="mb-12">{children}</h1>,
-  heading2: ({ children }) => <h2 className="mb-12">{children}</h2>,
+  paragraph: ({ children }) => <p className="mb-6 text-md">{children}</p>,
+  heading1: ({ children }) => <h1 className="font-bold mb-6">{children}</h1>,
+  heading2: ({ children }) => (
+    <h2 className="text-2xl font-bold mb-6">{children}</h2>
+  ),
+  heading3: ({ children }) => (
+    <h3 className="text-xl  font-bold mb-6">{children}</h3>
+  ),
+  list: ({ children }) => <ul className="mb-6">{children}</ul>,
+  listItem: ({ children }) => <li className="mb-2">{children}</li>,
 };
 
 export default function RichText({ slice }) {
