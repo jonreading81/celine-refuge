1991a69853468698fedadb360b60cfdc980cbb74
Jon Reading <jon.reading@mixcloud.com>
2024-02-23 16:59:26 +0000

    chore: add padding to slice wrapper

--- src/app/components/SliceWrapper/index.tsx
+++ src/app/components/SliceWrapper/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export const SliceWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-7xl mx-auto mt-[-120px] relative bg-white px-8 sm:px-20">
+    <div className="max-w-7xl mx-auto mt-[-120px] relative bg-white px-8 sm:px-20 pt-6">
       {children}
     </div>
   );
