4c360e79af77f59f827cb02bc78d026128725610
Jon Reading <jon.reading@mixcloud.com>
2024-03-11 06:33:57 +0000

    chore: update types for env

---
+++ environment.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    readonly NEXT_PUBLIC_GOOGLE_MAPS_API: string;
+    readonly RESEND_APIKEY: string;
+    readonly NEXT_PUBLIC_PRISMIC_ENVIRONMENT: string;
+  }
+}
