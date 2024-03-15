aa823a703643ec74f09f0107066d3901d3e52ec3
Jon Reading <jon.reading@mixcloud.com>
2024-03-15 07:55:37 +0000

    chore: styling amends to rich text

--- src/slices/RichText/index.jsx
+++ src/slices/RichText/index.jsx
@@ -9,7 +9,7 @@ export default function RichText({ slice }) {
     <section
       className={`${styles.wrapper} flex justify-center items-center mb-12`}
     >
-      <div className="max-w-[840px]">
+      <div className="w-full max-w-[840px]">
         <PrismicRichText
           field={slice.primary.content}
           components={components}
