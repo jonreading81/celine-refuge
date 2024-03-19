482a483387fe99c262842177c58a23d4482cfb2e
Jon Reading <jon.reading@mixcloud.com>
2024-03-19 23:28:38 +0000

    chore: header spacing

--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -5,7 +5,7 @@ import { LanguageSwitcher } from '@app/components/LanguageSwitcher';
 const Header = ({ navigationSlice, locales, lang }) => (
   <>
     <header className="w-full sticky top-0 z-50 bg-white shadow-xl">
-      <div className="mx-auto max-w-screen-2xl  px-4 py-2 ">
+      <div className="mx-auto max-w-screen-2xl  px-4 pt-2 pb-4 lg:py-6 ">
         <div className="w-full">
           <Navigation slice={navigationSlice} locales={locales} lang={lang} />
         </div>
