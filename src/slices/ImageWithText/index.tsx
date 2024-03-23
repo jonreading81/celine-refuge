2fdbdd00f374787221635a9a6ec993ac496d98e3
Jon Reading <jon.reading@mixcloud.com>
2024-03-23 15:38:07 +0000

    fix: spacing on iimageAndText

--- src/slices/ImageWithText/index.tsx
+++ src/slices/ImageWithText/index.tsx
@@ -24,12 +24,14 @@ const ImageWithText = ({
 }: ImageWithTextProps): JSX.Element => (
   <Slice>
     <div className="m-auto max-w-screen-xl lg:grid grid-cols-2 gap-8">
-      <PrismicNextImage
-        className={`w-full rounded-lg shadow-lg aspect-video  lg:aspect-[4/3] object-cover object-center mb-4 lg:mb-0 ${
-          alignment === 'Image right' ? 'lg:order-last' : ''
-        }`}
-        field={image}
-      />
+      <div className="w-full my-auto">
+        <PrismicNextImage
+          className={`w-full rounded-lg shadow-lg aspect-video  lg:aspect-[4/3] object-cover object-center mb-6 lg:mb-0 ${
+            alignment === 'Image right' ? 'lg:order-last' : ''
+          }`}
+          field={image}
+        />
+      </div>
       <div className="m-auto">
         <PrismicRichText field={text} components={components} />
       </div>
