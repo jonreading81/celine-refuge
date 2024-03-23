cbd033344b68ed9919b94a46abe4c4fc1f260614
Jon Reading <jon.reading@mixcloud.com>
2024-03-23 12:43:04 +0000

    chore: ensure home link goes to correct locale

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -1,17 +1,22 @@
 import Image from 'next/image';
 import logo from './logo.svg';
 import Link from 'next/link';
+import { useLocale } from '@app/hooks/useLocale';
 
-export const Logo = () => (
-  <h2>
-    <Link href="/">
-      <Image
-        className="w-[220px] lg:w-[300px] "
-        priority
-        src={logo}
-        alt="Follow us on Twitter"
-      />
-      <span className="sr-only">Refuge du Suffet</span>
-    </Link>
-  </h2>
-);
+export const Logo = () => {
+  const { locales, lang } = useLocale();
+
+  return (
+    <h2>
+      <Link href={`/${lang === 'fr-fr' ? '' : lang}`} locale={lang}>
+        <Image
+          className="w-[220px] lg:w-[300px] "
+          priority
+          src={logo}
+          alt="Refuge du Suffet logo"
+        />
+        <span className="sr-only">Refuge du Suffet</span>
+      </Link>
+    </h2>
+  );
+};
