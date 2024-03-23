2d89bf30d2362345497ac366a179e7106b497c63
Jon Reading <jon.reading@mixcloud.com>
2024-03-23 11:44:39 +0000

    chore: fix rich text

--- src/slices/RichText/index.jsx
+++ src/slices/RichText/index.jsx
@@ -4,16 +4,14 @@ import { components } from '@app/utils/richTextComponents';
 
 import * as styles from './styles.module.css';
 
-export default function RichText({ slice })  (
-    <section
-      className={`${styles.wrapper} flex justify-center items-center mb-12`}
-    >
-      <div className="w-full max-w-prose">
-        <PrismicRichText
-          field={slice.primary.content}
-          components={components}
-        />
-      </div>
-    </section>
-  );
+const RichText = ({ slice }) => (
+  <section
+    className={`${styles.wrapper} flex justify-center items-center mb-12`}
+  >
+    <div className="w-full max-w-prose">
+      <PrismicRichText field={slice.primary.content} components={components} />
+    </div>
+  </section>
+);
 
+export default RichText;
