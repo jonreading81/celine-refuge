df47e50d5612fc204fcdae3e8e6148fc54d3c3cd
Jon Reading <jon.reading@mixcloud.com>
2024-03-29 10:27:56 +0000

    chore: add languages to meta data

--- src/app/utils/generateMetadataByPage.ts
+++ src/app/utils/generateMetadataByPage.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@/prismicio';
+import { createClient, linkResolver } from '@/prismicio';
 import { notFound } from 'next/navigation';
 import * as prismic from '@prismicio/client';
 
@@ -10,7 +10,16 @@ export const generateMetadataForPage =
       .getByUID('page', name, { lang })
       .catch(() => notFound());
 
+    const alternate_languages = page.alternate_languages.reduce(
+      (pages, page) => ({
+        [page.lang]: linkResolver(page),
+        ...pages,
+      }),
+      {}
+    );
+
     return {
+      metadataBase: process.env.NEXT_PUBLIC_SITE_URL,
       title: prismic.asText(page.data.title),
       description: page.data.meta_description,
       keywords: page.tags,
@@ -21,6 +30,10 @@ export const generateMetadataForPage =
             url: page.data.meta_image.url || '',
           },
         ],
+      },
+      alternates: {
+        canonical: linkResolver(page),
+        languages: alternate_languages,
       },
     };
   };
