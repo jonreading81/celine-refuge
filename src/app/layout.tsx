6d7a071e82c4f5121e65a4bfe4e258dd78bb8dc3
Jon Reading <jon.reading@mixcloud.com>
2024-04-02 20:34:30 +0100

    chore: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default async function RootLayout({
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   // Don’t change the URL of Next.js assets starting with _next
-  matcher: ['/((?!_next).*)'],
+  matcher: '/((?!.*\\.|api\\/).*)',
 };
