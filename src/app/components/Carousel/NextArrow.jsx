144094e2a0607c8c26383d0d46ac34f539467359
Jon Reading <jon.reading@mixcloud.com>
2024-04-08 20:38:51 +0100

    chore: add accessible buttons

---
+++ src/app/components/Carousel/NextArrow.jsx
@@ -0,0 +1,29 @@
+import { IconButton } from '@material-tailwind/react';
+
+const NextArrow = ({ handleNext }) => (
+  <IconButton
+    variant="text"
+    color="white"
+    size="lg"
+    onClick={handleNext}
+    className="!absolute top-2/4 !right-4 -translate-y-2/4 rounded-full"
+    aria-label="Next"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+      />
+    </svg>
+  </IconButton>
+);
+
+export default NextArrow;
--- src/app/components/Carousel/index.jsx
+++ src/app/components/Carousel/index.jsx
@@ -1,13 +1,20 @@
 'use client';
-import * as React from 'react';
+
 import {
   Carousel as TailwindCarousel,
-  CarouselProps,
   IconButton,
 } from '@material-tailwind/react';
 
+import PreviousArrow from './PreviousArrow';
+import NextArrow from './NextArrow';
+
 const Carousel = ({ children, placeholder, ...props }) => (
-  <TailwindCarousel placeholder={placeholder} {...props}>
+  <TailwindCarousel
+    placeholder={placeholder}
+    prevArrow={PreviousArrow}
+    nextArrow={NextArrow}
+    {...props}
+  >
     {children}
   </TailwindCarousel>
 );
