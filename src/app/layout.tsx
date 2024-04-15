4bbc1a49af8311e983e31bb42cbec3b315edd581
Jon Reading <jon.reading@mixcloud.com>
2024-04-15 09:22:11 +0100

    chore: fix nested html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning>
+    <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
